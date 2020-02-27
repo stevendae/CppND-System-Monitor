102f3670cb1d0f9940ab69612c11bdeb8f235204
stevendae <steve.lee182@gmail.com>
2020-02-27 13:46:50 -0500

    Processor private variables added

--- include/processor.h
+++ include/processor.h
@@ -7,6 +7,22 @@ class Processor {
 
   // TODO: Declare any necessary private members
  private:
+    unsigned long int PrevIdle {0};
+    unsigned long int Idle;
+
+    unsigned long int PrevNonIdle {0};
+    unsigned long int NonIdle;
+
+    unsigned long int PrevTotal{0};
+    unsigned long int Total;
+
+    unsigned long int totald;
+    unsigned long int idled;
+
+    float CPU_Percentage;
+
+
+
 };
 
 #endif
