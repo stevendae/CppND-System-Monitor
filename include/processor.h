b541ffb771d4ccb46ebd15524fdb4fff7cd89ab3
stevendae <steve.lee182@gmail.com>
2020-02-27 17:27:13 -0500

    Updated processor source code in system, processor source and header

--- include/processor.h
+++ include/processor.h
@@ -1,28 +1,17 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
+#include <vector>
+#include <string>
+
 class Processor {
  public:
   float Utilization();  // TODO: See src/processor.cpp
 
   // TODO: Declare any necessary private members
  private:
-    unsigned long int PrevIdle {0};
-    unsigned long int Idle;
-
-    unsigned long int PrevNonIdle {0};
-    unsigned long int NonIdle;
-
-    unsigned long int PrevTotal{0};
-    unsigned long int Total;
-
-    unsigned long int totald;
-    unsigned long int idled;
-
-    float CPU_Percentage;
-
-
-
+    std::vector<std::string> prev_cpu{};
+    std::vector<std::string> curr_cpu{};
 };
 
 #endif
