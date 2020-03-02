1eb814795f3a1ab894ad6633a561d9c861ee6119
stevendae <steve.lee182@gmail.com>
2020-03-01 21:10:11 -0500

    Implemented sorting algorithm for vector of processes as well as added member variables to process

--- src/system.cpp
+++ src/system.cpp
@@ -15,12 +15,43 @@ using std::string;
 using std::vector;
 
 // TODO: Return the system's CPU
-Processor& System::Cpu() {
-    return cpu_;
-}
+Processor& System::Cpu() { return cpu_; }
 
 // TODO: Return a container composed of the system's processes
-vector<Process>& System::Processes() { return processes_; }
+// Ordered based on CpuUtilization
+vector<Process>& System::Processes() { 
+    
+    vector<int> pids{LinuxParser::Pids()};
+
+    // Create a set
+    // Go through extant (still in existance) Processes
+    set<int> extant_pids;
+    for (Process &process : processes_) {
+        extant_pids.insert(process.Pid());
+    }
+
+    // Emplace (put into place) all new processes
+    for (int pid : pids) {
+        // set.find(val) searches the container for an element equivalent to val
+        // and returns an iterator to it if found, otherwise it returns an
+        // iterator to set::end
+
+        // this is asking is the pid in the PID vector an existing PID,
+        // if not than add it to the end of processes vector
+        if (extant_pids.find(pid) == extant_pids.end()){
+            Process process(pid);
+            processes_.push_back(process);
+        }
+    }
+
+    // Update CPU utilization
+    for (auto& process : processes_) {
+        process.CpuUtilization();
+    }
+    
+    std::sort(processes_.begin(), processes_.end(), std::less<Process>());
+    return processes_; 
+    }
 
 // TODO: Return the system's kernel identifier (string)
 std::string System::Kernel() { return LinuxParser::Kernel(); }
