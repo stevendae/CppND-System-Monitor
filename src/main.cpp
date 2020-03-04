c84c937774fbd2f277937da64c8096f8fd534ddf
stevendae <steve.lee182@gmail.com>
2020-03-04 13:02:46 -0500

    Tester code to determine why cpu usage is zero

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include "ncurses_display.h"
 #include "system.h"
 
+#include <vector>
+#include "process.h"
+#include <iostream>
+#include "string.h"
+
 int main() {
   System system;
+  //std::vector <Process> test = system.Processes();
+  //float cpu = test[0].CpuUtilization();
+  //std::cout << cpu << std::endl;
   NCursesDisplay::Display(system);
 }
